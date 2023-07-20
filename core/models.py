efb3dd99e003bc8bf93dc384d0d7174ac5f50781
AlStien <aj601604@gmail.com>
2023-07-20 18:17:03 +0530

    added initial models and views

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Elevator(models.Model):
+    is_operational = models.BooleanField(default=True)
+    current_floor = models.PositiveIntegerField(default=1)
+    is_moving_up = models.BooleanField(default=False)
+    is_moving_down = models.BooleanField(default=False)
+    is_door_open = models.BooleanField(default=False)
+
+class UserRequest(models.Model):
+    elevator = models.ForeignKey(Elevator, on_delete=models.CASCADE, related_name='requests')
+    from_floor = models.PositiveIntegerField()
+    to_floor = models.PositiveIntegerField()
+    requested_time = models.DateTimeField(auto_now_add=True)
+    priority = models.PositiveIntegerField(default=0)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Elevator, UserRequest
+from .serializers import ElevatorSerializer, UserRequestSerializer
 
-# Create your views here.
+class ElevatorViewSet(viewsets.ModelViewSet):
+    queryset = Elevator.objects.all()
+    serializer_class = ElevatorSerializer
+    
+class UserRequestViewSet(viewsets.ModelViewSet):
+    queryset = UserRequest.objects.all()
+    serializer_class = UserRequestSerializer
+
