86ab42721a8d156f3bd2f07ed7b4e594be4ab0d3
AlStien <aj601604@gmail.com>
2023-07-21 18:54:24 +0530

    updated admin to display all information

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from . import models
 
+class CustomModelAdmin(admin.ModelAdmin):
+    
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        super(CustomModelAdmin, self).__init__(model, admin_site)
+
+class ElevatorSystemAdmin(CustomModelAdmin):
+    pass
+
+class ElevatorAdmin(CustomModelAdmin):
+    pass
+
+class UserRequestAdmin(CustomModelAdmin):
+    pass
+
 # Register your models here.
-admin.site.register(models.Elevator)
-admin.site.register(models.UserRequest)
+admin.site.register(models.ElevatorSystem, ElevatorSystemAdmin)
+admin.site.register(models.Elevator, ElevatorAdmin)
+admin.site.register(models.UserRequest, UserRequestAdmin)
